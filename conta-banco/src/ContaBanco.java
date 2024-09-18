1bd0ccdfc86aeeb5619d8abb79144d1df70c1906
gabs <1gabrielbprado@gmail.com>
2024-09-18 03:08:34 -0400

    updated vers

--- conta-banco/src/ContaBanco.java
+++ conta-banco/src/ContaBanco.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class ContaBanco {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.println("Por favor, digite o numero da nova Conta !");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o numero da Agencia !");
+        scanner.nextLine();
+        String numeroAgencia = scanner.nextLine(); 
+
+        System.out.println("Por favor, digite o seu Nome Completo !");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o valor do deposito inicial na sua nova conta bancaria !");
+        double primeiroSaldo = scanner.nextDouble();
+            DecimalFormat df = new DecimalFormat("#.##");
+            String saldoFormatado = df.format(primeiroSaldo);
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + numeroAgencia + ", conta " + numeroConta + " e seu saldo " + primeiroSaldo + " já está disponível para saque.");
+
+        scanner.close();
     }
+    
 }
